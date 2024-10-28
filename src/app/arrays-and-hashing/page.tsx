ce3e771f655bfcff0cafae55d97215be5feeff13
Lucas Drieberg <lucas.drieberg@au.ey.com>
2024-10-28 21:50:25 +1100

    is anagram

--- src/app/arrays-and-hashing/page.tsx
+++ src/app/arrays-and-hashing/page.tsx
@@ -4,11 +4,15 @@ import { useEffect } from "react";
 export default function ArraysAndHashing() {
 
     useEffect(() => {
-        console.log("using a hashmap: " + findDuplicates(nums))
-        console.log("using a set: " + findDuplicatesUsingSet(nums))
+        // console.log("using a hashmap: " + findDuplicates(nums))
+        // console.log("using a set: " + findDuplicatesUsingSet(nums))
+
+        // console.log(isAnogram(s, t));
     }, [])
 
     const nums = [1, 2, 4, 5, 7, 10, 20,];
+    const s = "racecar"
+    const t = "racecar"
 
     const findDuplicates = (nums: number[]) => {
         const hashMap = {};
@@ -37,9 +41,24 @@ export default function ArraysAndHashing() {
         return false;
     }
 
+    const isAnogram = (s: string, t: string) => {
+        if (s.length != t.length) {
+            return false;
+        }
+
+        return s.split("").sort().join("") === t.split("").sort().join("");
+    }
+
+
+
     return (
         <div>
             <h1>Arrays and Hashing</h1>
+            <ul className="list-disc">
+                <li>find duplicates</li>
+                <li>is anogram</li>
+                <li></li>
+            </ul>
         </div>
     );
 }
