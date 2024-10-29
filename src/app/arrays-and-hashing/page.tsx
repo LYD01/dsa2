67f40b114cbaf5ad859f9b9811d29caa1e9df578
Lucas Drieberg <lucas.drieberg@au.ey.com>
2024-10-30 09:05:00 +1100

    two sum

--- src/app/arrays-and-hashing/page.tsx
+++ src/app/arrays-and-hashing/page.tsx
@@ -4,52 +4,79 @@ import { useEffect } from "react";
 export default function ArraysAndHashing() {
 
     useEffect(() => {
-        // console.log("using a hashmap: " + findDuplicates(nums))
-        // console.log("using a set: " + findDuplicatesUsingSet(nums))
+
+
+        // console.log(twoSum(nums, target))
 
         // console.log(isAnogram(s, t));
+
+        // console.log("using a hashmap: " + findDuplicates(nums))
+        // console.log("using a set: " + findDuplicatesUsingSet(nums))
     }, [])
 
-    const nums = [1, 2, 4, 5, 7, 10, 20,];
-    const s = "racecar"
-    const t = "racecar"
 
-    const findDuplicates = (nums: number[]) => {
-        const hashMap = {};
-        for (let i = 0; i < nums.length; i++) {
-            const num = nums[i];
+    // 3 
+    // const nums = [3, 4, 5, 6]
+    // const target = 10 // 7
+    // const twoSum = (nums: number[], target: number) => {
+    //     const map = {};
 
-            // if elem has already been seen
-            if (hashMap[num]) {
-                return true;
-            }
-            // otherwise add elem to object
-            hashMap[num] = true;
-        }
-        return false;
-    }
+    //     for (let i = 0; i < nums.length; i++) {
+    //         const num = nums[i];
+    //         const complement = target - num
 
-    const findDuplicatesUsingSet = (nums: number[]) => {
-        const seen = new Set;
+    //         if (map[complement] != undefined) {
+    //             // found the sum
+    //             return [map[complement], i];
+    //         }
 
-        for (const num of nums) {
-            if (seen.has(num)) {
-                return true
-            }
-            seen.add(num);
-        }
-        return false;
-    }
-
-    const isAnogram = (s: string, t: string) => {
-        if (s.length != t.length) {
-            return false;
-        }
-
-        return s.split("").sort().join("") === t.split("").sort().join("");
-    }
+    //         // We’re storing each number’s index in the map so that later, if we find another number that adds with num to meet the target, we can easily retrieve the index of this stored num using the map.
+    //         /*
+    //             1. map[num]: This is where we use the value of the current number (num) as a key in the map (the hash map or JavaScript object).
+    //             2. = i: We set the value for that key (map[num]) to be the index (i) of num in the nums array.
+    //         */
+    //         map[num] = i;
+    //     }
+    // }
 
 
+    // 2.
+    // const s = "racecar"
+    // const t = "racecar"
+    // const isAnogram = (s: string, t: string) => {
+    //     if (s.length != t.length) {
+    //         return false;
+    //     }
+    //     return s.split("").sort().join("") === t.split("").sort().join("");
+    // }
+
+    // 1.
+    // const nums = [1, 2, 4, 5, 7, 10, 20,];
+    // const findDuplicates = (nums: number[]) => {
+    //     const hashMap = {};
+    //     for (let i = 0; i < nums.length; i++) {
+    //         const num = nums[i];
+
+    //         // if elem has already been seen
+    //         if (hashMap[num]) {
+    //             return true;
+    //         }
+    //         // otherwise add elem to object
+    //         hashMap[num] = true;
+    //     }
+    //     return false;
+    // }
+    // const findDuplicatesUsingSet = (nums: number[]) => {
+    //     const seen = new Set;
+
+    //     for (const num of nums) {
+    //         if (seen.has(num)) {
+    //             return true
+    //         }
+    //         seen.add(num);
+    //     }
+    //     return false;
+    // }
 
     return (
         <div>
@@ -57,7 +84,7 @@ export default function ArraysAndHashing() {
             <ul className="list-disc">
                 <li>find duplicates</li>
                 <li>is anogram</li>
-                <li></li>
+                <li>two sum</li>
             </ul>
         </div>
     );
