2599e0fb665d316ca43b442a186c47333816ac33
Lucas Drieberg <lucas.drieberg@au.ey.com>
2025-01-10 09:14:49 +1100

    two sum 2

--- src/app/two-pointers/page.tsx
+++ src/app/two-pointers/page.tsx
@@ -2,54 +2,66 @@ export default function TwoPointers() {
 
 
 
+    // 2. 
+    // const numbers = [1, 2, 3, 4]
+    // const target = 3
+    // function twoSum2(numbers: number[], target: number) {
+    //     let left = 0
+    //     let right = numbers.length - 1
 
+    //     while (left < right) {
 
+    //         const curSum = numbers[left] + numbers[right]
 
-
-
-
-
-
-
+    //         if (curSum > target) {
+    //             right -= 1
+    //         } else if (curSum < target) {
+    //             left += 1
+    //         } else {
+    //             return [left + 1, right + 1]
+    //         }
+    //     }
+    //     return []
+    // }
+    // console.log(twoSum2(numbers, target))
 
 
 
     // 1. 
+    // const str = "race car?"
+    // function isValidPalendrone(str: string) {
+    //     let left = 0
+    //     let right = str.length - 1
+    //     const cleanStr = str.toLowerCase().replaceAll(" ", "")
+    //     console.log(cleanStr)
+    //     function isAlpha(char: string) {
+    //         return (
+    //             (char >= 'a' && char <= 'z') ||
+    //             (char >= 'A' && char <= 'Z') ||
+    //             (char >= '0' && char <= '9')
+    //         );
+    //     }
 
-    const str = "race car?"
-    function isValidPalendrone(str: string) {
-        let left = 0
-        let right = str.length - 1
-        const cleanStr = str.toLowerCase().replaceAll(" ", "")
-        console.log(cleanStr)
-        function isAlpha(char: string) {
-            return (
-                (char >= 'a' && char <= 'z') ||
-                (char >= 'A' && char <= 'Z') ||
-                (char >= '0' && char <= '9')
-            );
-        }
+    //     while (left < right) {
+    //         if (!isAlpha(cleanStr[left])) {
+    //             left++
+    //         }
 
-        while (left < right) {
-            if (!isAlpha(cleanStr[left])) {
-                left++
-            }
+    //         if (!isAlpha(cleanStr[right])) {
+    //             right--
+    //         }
 
-            if (!isAlpha(cleanStr[right])) {
-                right--
-            }
+    //         else if (cleanStr[left] !== cleanStr[right]) {
+    //             return false
+    //         } else {
+    //             left++
+    //             right--
+    //         }
+    //     }
+    //     return true;
+    // }
 
-            else if (cleanStr[left] !== cleanStr[right]) {
-                return false
-            } else {
-                left++
-                right--
-            }
-        }
-        return true;
-    }
-
-    console.log(isValidPalendrone(str))
+    // console.log(isValidPalendrone(str))
 
 
 
