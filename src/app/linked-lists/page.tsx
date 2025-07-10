0bcb43629dd8c9062f041b035eb985b25feaeec5
Lucas Drieberg <lucas.drieberg@au.ey.com>
2025-07-10 14:58:53 +1000

    add linked lists

---
+++ src/app/linked-lists/page.tsx
@@ -0,0 +1,30 @@
+export default function LinkedLists() {     
+
+
+
+
+    /*
+    
+    */
+    // const head = [0,1,2,3]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return (
+       <ul className="m-4 pl-4 list-disc">
+            <li>reverse linked list</li>
+       </ul>
+    );
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,37 @@
-import Image from "next/image";
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
+  const pathname = usePathname();
+
+ const routes = [
+          { path: "/arrays-and-hashing", label: "arrays and hashing" },
+          { path: "/binary-search", label: "binary search" },
+          { path: "/debounce-and-throttle", label: "debounce and throttle" },
+          { path: "/merge-sort", label: "merge sort" },
+          { path: "/sliding-window", label: "sliding window" },
+          { path: "/stack", label: "stack" },
+          { path: "/two-pointer", label: "two pointer" },
+          { path: "/linked-lists", label: "linked lists" }, 
+        ];
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <nav>
+            <ul className="flex gap-6">
+              {routes.map((route) => (
+                <li key={route.path}>
+                  <Link
+                    href={route.path}
+                    className={pathname === route.path ? "font-bold underline" : ""}
+                  >
+                    {route.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
     </div>
   );
 }
